4363b972a40c1988e4a7b56c418e049f5db5f24d
harttle <yangjvn@126.com>
2017-09-05 18:38:44 +0800

    fix lint

--- test-main.js
+++ test-main.js
@@ -1,29 +1,34 @@
-var allTestFiles = []
-var TEST_REGEXP = /(spec|test)\.js$/i
+/**
+ * @file testMain test entry for karma
+ * @author harttle<yangjvn@126.com>
+ */
+var allTestFiles = [];
+var TEST_REGEXP = /(spec|test)\.js$/i;
 
 // Get a list of all the test files to include
 Object.keys(window.__karma__.files).forEach(function (file) {
-  if (TEST_REGEXP.test(file)) {
-    // Normalize paths to RequireJS module names.
-    // If you require sub-dependencies of test files to be loaded as-is (requiring file extension)
-    // then do not normalize the paths
-    var normalizedTestModule = file.replace(/^\/base\/|\.js$/g, '')
-    allTestFiles.push(normalizedTestModule)
-  }
-})
+    if (TEST_REGEXP.test(file)) {
+        // Normalize paths to RequireJS module names.
+        // If you require sub-dependencies of test files to be loaded as-is (requiring file extension)
+        // then do not normalize the paths
+        var normalizedTestModule = file.replace(/^\/base\/|\.js$/g, '');
+        allTestFiles.push(normalizedTestModule);
+    }
+
+});
 
 require.config({
-  // Karma serves files under /base, which is the basePath from your config file
-  baseUrl: '/base',
+    // Karma serves files under /base, which is the basePath from your config file
+    baseUrl: '/base',
 
-  // dynamically load all test files
-  deps: allTestFiles,
+    // dynamically load all test files
+    deps: allTestFiles,
 
-  // external AMD modules
-  paths: {
-    '@searchfe/assert': 'amd_modules/@searchfe/assert'
-  },
+    // external AMD modules
+    paths: {
+        '@searchfe/assert': 'amd_modules/@searchfe/assert'
+    },
 
-  // we have to kickoff jasmine, as it is asynchronous
-  callback: window.__karma__.start
-})
+    // we have to kickoff jasmine, as it is asynchronous
+    callback: window.__karma__.start
+});
