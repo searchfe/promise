68a3a2e92bc4f31c00cb2148a3a94882f109e295
harttle <harttle@harttle.com>
2017-10-10 18:45:11 +0800

    modified:   karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -94,6 +94,7 @@ module.exports = function (config) {
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
         singleRun: true,
+        captureTimeout: 120000,
 
         // Concurrency level
         // how many browser should be started simultaneous
